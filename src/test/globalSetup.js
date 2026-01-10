557f21c22b23675983b02d9c10fa979739b584d3
aniTech-addict <kirti7666anand15@gmail.com>
2026-01-10 10:34:51 +0530

    feat(posts): add post creation service and tests
    - Introduce `createPost` service in `src/services/posts.service.js` to handle post creation using

--- src/test/globalSetup.js
+++ src/test/globalSetup.js
@@ -1,11 +1,11 @@
 import { MongoMemoryServer } from 'mongodb-memory-server'
 
-export default globalSetupFunction () {
-    const instance = MongoMemoryServer.create({
+export default async function globalSetup() {
+    const instance = await MongoMemoryServer.create({
         binary: {
             version: '6.0.4', // same version as Docker
         },
-        global.__MONGOINSTANCE = instance
-        process.env.DATABASE_URL = instance.getUri()
     })
+    global.__MONGOINSTANCE = instance
+    process.env.DATABASE_URL = instance.getUri()
 }
