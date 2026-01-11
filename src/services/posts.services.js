038ff7731e611c3fe0ad834e8523bd4b2346567e
aniTech-addict <kirti7666anand15@gmail.com>
2026-01-11 12:22:20 +0530

    feat: retrive all post with query

--- src/services/posts.services.js
+++ src/services/posts.services.js
@@ -1,6 +1,6 @@
 import Post from '../db/models/posts.model.js'
 
-export async function createPost({ title, author, contents, tags }) {
+async function createPost({ title, author, contents, tags }) {
     const post = new Post({
         title,
         author,
@@ -8,4 +8,33 @@ export async function createPost({ title, author, contents, tags }) {
         tags,
     })
     return await post.save()
+}
+
+async function listPosts({
+    query = {},
+    sortBy = 'createdAt',
+    sortOrder = 'descending'
+} = {}) {
+    return await Post.find(query).sort({ [sortBy]: sortOrder })
+}
+
+
+async function listAllPosts(){
+    return await listPosts({})
+}
+
+async function listPostsByAuthor(author, options){
+    return await listPosts({ query: { author } }, options);
+}
+
+async function listPostsByTag(tags, options) {
+return await listPosts({ query: { tags } }, options)
+}
+
+
+export {
+    createPost,
+    listAllPosts,
+    listPostsByTag,
+    listPostsByAuthor
 }
