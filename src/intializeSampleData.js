1f9f5596db89083cd9f5740f13da488c246b3668
aniTech-addict <kirti7666anand15@gmail.com>
2026-01-11 17:18:45 +0530

    feat(posts): add script to initialize sample data

---
+++ src/intializeSampleData.js
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import { initDB } from './db/init.js'
+import Post  from './db/models/posts.model.js'
+
+await initDB()
+
+const post = new Post({
+    title: 'Nigga',
+    author: 'Daniel',
+    contents: 'one nigga pollute whole nigga gang',
+    tags: ['niggas', 'black'],
+})
+
+await post.save()
+
+const posts = await Post.find()
+console.log(posts)
